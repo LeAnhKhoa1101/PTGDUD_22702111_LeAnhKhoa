d2190b934c23fb97338c2a9d6c422d78589cdeea
H82M19\Student <Student>
2026-01-21 13:33:10 +0700

    small change

--- 22702111_LeAnhKhoa_PTGDUD/src/App.jsx
+++ 22702111_LeAnhKhoa_PTGDUD/src/App.jsx
@@ -15,8 +15,8 @@ function App() {
         {/* Truyền type="danger" -> Nút màu đỏ */}
         <Button type="danger">Xóa bỏ</Button>
 
-        {/* Truyền type="success" -> Nút màu xanh lá */}
-        <Button type="success">Thành công</Button>
+       
+        
       </div>
     </div>
   )
